67a605358053d50154038a06f2fd8a2c929522c7
John Lago <john.lago@xamarin.com>
2017-01-26 17:43:00 -0800

    fix #6 by initializing DataController on AppLaunch

--- swift/Swfitnotes/Swfitnotes/AppDelegate.swift
+++ swift/Swfitnotes/Swfitnotes/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let _  = DataController.sharedInstance
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let leftViewController = splitViewController.viewControllers.first as! UINavigationController
         let masterViewController = leftViewController.topViewController as! SwiftNoteCollectionViewController
