52cf0450a942e08a86eca0a31dba6dae4558942e
Simon Søndergaard <sisonder@microsoft.com>
2017-02-02 14:09:21 +0100

    Adding test written by hand. I think its more legible and can be run more that once. The original test would fail is run more that one if it was not for the static title

--- app/src/androidTest/java/com/moonpi/swiftnotes/testCreateNote.java
+++ app/src/androidTest/java/com/moonpi/swiftnotes/testCreateNote.java
@@ -1,6 +1,5 @@
 package com.moonpi.swiftnotes;
 
-
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,21 +8,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.UUID;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
-import android.support.test.runner.AndroidJUnit4;
-import com.xamarin.testcloud.espresso.Factory;
-import com.xamarin.testcloud.espresso.ReportHelper;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -88,6 +103,55 @@ public class testCreateNote {
                         isDisplayed()));
         textView.check(matches(withText("My note")));
 
+    }
+
+    @Test
+    public void testCreateNoteHandCurated() {
+        reportHelper.label("MainScreen");
+
+        onView(withId(R.id.newNote)).perform(click()).check(doesNotExist());
+
+        reportHelper.label("NewNote");
+
+        ViewInteraction title = onView(withId(R.id.titleEdit));
+        title.perform(click()).check(matches(hasFocus()));
+
+        reportHelper.label("Ready to change title");
+
+        // failed test showed that we only have 30 chars for title
+        String uniqTitle = UUID.randomUUID().toString().substring(0, 29);
+        title.perform(replaceText(uniqTitle), closeSoftKeyboard());
+
+        reportHelper.label("Replaced title");
+
+        onView(withId(R.id.bodyEdit)).perform(replaceText("My note content"), closeSoftKeyboard());
+
+        onView(allOf(withContentDescription("Navigate up"), withParent(withId(R.id.toolbarEdit))))
+                .perform(click());
+
+        onView(withText(R.string.yes_button)).check(matches(isDisplayed()))
+            .perform(click());
+
+        reportHelper.label("Back on main screen");
+
+        onData(allOf(is(instanceOf(JSONObject.class)), hasTitle(uniqTitle)))
+                .check(matches(withChild(allOf(withId(R.id.titleView), withText(uniqTitle), isDisplayed()))));
+        reportHelper.label("My newly created note is in the list");
+    }
+
+    private Matcher<JSONObject> hasTitle(final String title) {
+        return new TypeSafeMatcher<JSONObject>() {
+            @Override
+            protected boolean matchesSafely(JSONObject item) {
+                return item.has(DataUtils.NOTE_TITLE) &&
+                        title.contentEquals(item.optString(DataUtils.NOTE_TITLE));
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText(String.format("hasTitle %s", title));
+            }
+        };
     }
 
     private static Matcher<View> childAtPosition(
