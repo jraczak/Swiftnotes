56d2264ede362d690670dc3995f7b433558d19f3
Justin Raczak <jraczak@gmail.com>
2017-02-13 18:15:48 -0800

    Label the rest of the test steps

--- app/src/androidTest/java/com/moonpi/swiftnotes/testCreateNote.java
+++ app/src/androidTest/java/com/moonpi/swiftnotes/testCreateNote.java
@@ -28,8 +28,12 @@ import static org.hamcrest.CoreMatchers.anyOf;
 import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
 
+import android.support.test.runner.AndroidJUnit4;
 import com.xamarin.testcloud.espresso.Factory;
 import com.xamarin.testcloud.espresso.ReportHelper;
+
+
+
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -41,47 +45,48 @@ public class testCreateNote {
     @Rule
     public ReportHelper reportHelper = Factory.getReportHelper();
 
+
     @Test
     public void testCreateFirstNote() {
 
-        // Add label for navigating to new note screen
+        reportHelper.label("Given I click new note");
         onView(withId(R.id.newNote)).perform(click());
 
-        // Add label for adding note title
+        reportHelper.label("Then I give the note a title");
         onView(withId(R.id.titleEdit)).perform(typeText("My first note"));
 
-        // Add label for adding note content
+        reportHelper.label("Then I give the note content");
         onView(withId(R.id.bodyEdit)).perform(typeText("Today I created my first note."));
 
-        // Add label for pressing the back button
+        reportHelper.label("Then I press the back button");
         onView(withContentDescription("Navigate up")).perform(click());
 
-        // Add label for saying "yes" to save dialog
+        reportHelper.label("And I confirm the save dialog");
         onView(withText("Yes")).perform(click());
 
-        // Add label for verifying the note was created
+        reportHelper.label("Then I should see the title of my note");
         onView(allOf(withText("My first note"))).check(matches(isDisplayed()));
     }
 
     @Test
     public void testCreateSecondNote() {
 
-        // Add label for navigating to new note screen
+        reportHelper.label("Given I click new note");
         onView(withId(R.id.newNote)).perform(click());
 
-        // Add label for adding note title
+        reportHelper.label("Then I give the note a title");
         onView(withId(R.id.titleEdit)).perform(typeText("Apples"));
 
-        // Add label for adding note content
+        reportHelper.label("Then I give the note content");
         onView(withId(R.id.bodyEdit)).perform(typeText("Oranges"));
 
-        // Add label for pressing the back button
+        reportHelper.label("When I navigate back");
         onView(withContentDescription("Navigate up")).perform(click());
 
-        // Add label for saying "yes" to save dialog
+        reportHelper.label("And I confirm the save dialog");
         onView(withText("Yes")).perform(click());
 
-        // Add label for verifying the note was created
+        reportHelper.label("Then I should see the title of my note");
         // Note that we expect this test to fail since "Bananas" was not part of the note content
         onView(allOf(withText("Bananas"))).check(matches(isDisplayed()));
     }
